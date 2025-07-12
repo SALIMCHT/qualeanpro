525ab055222f97fa607f745d710f1c5012274f05
SALIM CHATBI <saliimchatbii@gmail.com>
2025-07-12 21:40:47 +0200

    updates

--- src/main/java/com/qualeanpro/model/Contenu.java
+++ src/main/java/com/qualeanpro/model/Contenu.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "contenus") // convention snake_case pour les tables
+@Table(name = "contenu") // convention snake_case pour les tables
 public class Contenu {
 
     @Id
